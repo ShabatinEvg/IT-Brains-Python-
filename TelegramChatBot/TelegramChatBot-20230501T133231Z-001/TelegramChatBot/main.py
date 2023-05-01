119cf63ec9bc366032d0b5f6576986745cf0fead
ShabatinEvg <106232874+ShabatinEvg@users.noreply.github.com>
2023-05-01 17:50:31 +0300

    lesson_1

--- TelegramChatBot/TelegramChatBot-20230501T133231Z-001/TelegramChatBot/main.py
+++ TelegramChatBot/TelegramChatBot-20230501T133231Z-001/TelegramChatBot/main.py
@@ -1,6 +1,21 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
 
-BOT_TOKEN = ""
+BOT_TOKEN = "6131331187:AAFfBdg7Gjm4gBQ4KLrRBLADtNfsvzv3RGk"
 
-print("Hello!")
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text("Hello my bot! ;)")
+
+def run():
+    application = ApplicationBuilder().token(BOT_TOKEN).build()
+
+    application.add_handler(CommandHandler("Start", start))
+    application.add_handler(CommandHandler("Help", start))
+
+    application.run_polling()
+
+
+if __name__ == "__main__":
+    run()
